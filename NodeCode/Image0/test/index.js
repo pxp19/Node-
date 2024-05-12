a16f9c8735b9092a911ac357f5c56c3aee10e7e7
pxp19 <357783175@qq.com>
2024-05-12 22:00:25 +0800

    sharp处理图片学习

--- NodeCode/Image0/test/index.js
+++ NodeCode/Image0/test/index.js
@@ -1,6 +1,6 @@
 const sharp = require('sharp');
 const path = require('path');
-console.log(1);
+
 // console.log(path);
 console.log(path.resolve(__dirname, 'water.png'));
 console.log(path.resolve(__dirname, 'index.js'));
@@ -9,30 +9,27 @@ const imgPath = path.resolve(__dirname, 'water.png');
 async function getMetadata() {
   /* 错误示范
   const metadata = await sharp(water.png).metadata();
-    
-    */
-  //   sharp 模块需要传入完整的文件路径来读取图片
+ */
+  //   sharp 模块需要传入 # 完整的文件路径 # 来读取图片
   const metadata = await sharp(imgPath).metadata();
   console.log(metadata);
-
-  //   1. 修改大小
 }
-
 getMetadata();
 
-//
+//   1. 修改大小
 async function resizeImage() {
   try {
     await sharp('water.png')
+      // await sharp(imgPath)
       .resize({
         // 注意这个修改图片大小是需要按照一定比例来实现的 这个是原来图片大小的两倍
         width: 210,
-        heigh: 138,
+        height: 138,
       })
-      .toFile('water-resized.png');
+      .toFile('water-resized11.png');
     console.log('修改大小成功，请查看');
     const metadataResized = await sharp(
-      path.resolve(__dirname, 'water-resized.png')
+      path.resolve(__dirname, 'water-resized11.png')
     ).metadata();
     console.log(metadataResized);
   } catch (error) {
--- NodeCode/JS基础/test.js
+++ NodeCode/JS基础/test.js
@@ -1,4 +1,27 @@
-a = function () {
-  console.log(this);
-};
-a();
+// 模拟异步操作
+/*
+  promise 对象表示一个模拟的异步操作。
+  如果随机生成的数字大于 0.5，Promise 将被解析，
+  并且 then 方法的成功处理函数将被调用；
+  否则，
+  Promise 将被拒绝，并且 catch 方法的错误处理函数将被调用。
+  
+  */
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const randomNumber = Math.random();
+    if (randomNumber > 0.5) {
+      resolve(randomNumber); // 异步操作成功，将结果传递给 resolve 函数
+    } else {
+      reject(new Error('异步操作失败')); // 异步操作失败，将错误信息传递给 reject 函数
+    }
+  }, 1000);
+});
+
+promise
+  .then((result) => {
+    console.log('Promise 成功解决', result);
+  })
+  .catch((error) => {
+    console.error('Promise 失败处理', error);
+  });
